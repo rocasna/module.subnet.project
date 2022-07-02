71e87d4ecf371933f78ffba47308fc7945cae472
Roberto Castro <robertcas4@hotmail.com>
2022-07-02 22:27:51 +0200

    add output region subnet

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,6 @@ output "Subnet_SelfLink" {
 output "Subnet_Name" {
     value= google_compute_subnetwork.subnet.name
 }
+output "Region_Subnet" {
+    value= google_compute_subnetwork.subnet.region
+}
